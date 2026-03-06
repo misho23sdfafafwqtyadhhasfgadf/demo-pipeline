93dab3bed78e15568f9dabab3b11684c54ac1a5b
anyelo <anyelotaype@gmail.com>
2026-03-06 18:19:56 -0500

    primer commit pipeline node

---
+++ test/test.js
@@ -0,0 +1,8 @@
+const assert = require('assert');
+const saludar = require('../app');
+
+describe('Prueba de la función saludar', () => {
+    it('Debería saludar a Juan', () => {
+        assert.strictEqual(saludar('Juan'), 'Hola, Juan!');
+    });
+});
